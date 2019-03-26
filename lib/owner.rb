a7c46353b139d7d3575b99e68b480bafee69c002
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-03-26 09:16:04 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -63,7 +63,7 @@ end
 end
 
 def list_pets
- "I have #{pets[:fishes].count} fish, #{} dog(s), and #{} cat()."
+ "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat()."
 end
 
 def self.all
